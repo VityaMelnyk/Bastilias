7d6bc17303b45784e77679946aa078da1f46bec3
VityaMelnyk <vityamozer@gmail.com>
2020-09-06 14:44:41 +0300

    mama mila krishna

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("14 июля - взятия Бастилии");
         System.out.println("Java convention code");
+
+
+
     }
 }
